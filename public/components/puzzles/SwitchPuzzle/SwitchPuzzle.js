61df21cf58c74e4d3ac1620b9b729d18ac1f45b0
dbabusci <dbabusci@kent.edu>
2023-11-02 15:15:06 -0400

    iSolved() done + beginning of reset switch

--- public/components/puzzles/SwitchPuzzle/SwitchPuzzle.js
+++ public/components/puzzles/SwitchPuzzle/SwitchPuzzle.js
@@ -46,6 +46,26 @@ class flipSwitch{
   }
 }
 
+class resetSwitch{
+  constructor(flipped, x, y, w){
+    this.flipped = flipped; //always init to false
+    this.x = x;
+    this.y = y;
+    this.w = w/10;
+    this.h = w/5;
+  }
+
+  resetGame(){
+    this.flipped = true;
+    console.log("Reset able to be clicked");
+  }
+
+  drawResetSwitch(renderer){
+    renderer.text("Reset Game", this.x, this.y)
+    renderer.image(sOff, this.x, this.y, this.w, this.h);
+  }
+}
+
 class flipSwitchGame extends Puzzle{
   constructor(renderer,difficulty=0){
     super(renderer, difficulty)
@@ -63,54 +83,51 @@ class flipSwitchGame extends Puzzle{
       if(onOffArray[0] == true && onOffArray[1] == true && onOffArray[2] == true && onOffArray[3] == true && onOffArray[4] == true){
         randomChange = random([0,1,2,3,4]); //temp
         onOffArray[randomChange] = false;
-        console.log("Changed on-on-on-on-on");
+        //console.log("Changed on-on-on-on-on");
       }
       else if(onOffArray[0] == false && onOffArray[1] == false && onOffArray[2] == false && onOffArray[3] == false && onOffArray[4] == false){
         randomChange = random([0,1,2,3,4]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed off-off-off-off-off");
+        //console.log("Changed off-off-off-off-off");
       }
       else if(onOffArray[0] == true && onOffArray[1] == true && onOffArray[2] == false && onOffArray[3] == false && onOffArray[4] == false){
         randomChange = random([2,3,4]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed on-on-off-off-off");
+        //console.log("Changed on-on-off-off-off");
       }
       else if(onOffArray[0] == true && onOffArray[1] == false && onOffArray[2] == false && onOffArray[3] == false && onOffArray[4] == true){
         randomChange = random([1,2,3]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed om-off-off-off-on");
+        //console.log("Changed om-off-off-off-on");
       }
       else if(onOffArray[0] == false && onOffArray[1] == false && onOffArray[2] == false && onOffArray[3] == true && onOffArray[4] == true){
         randomChange = random([0,1,2]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed off-off-off-on-on");
+        //console.log("Changed off-off-off-on-on");
       }
       else if(onOffArray[0] == false && onOffArray[1] == false && onOffArray[2] == true && onOffArray[3] == true && onOffArray[4] == false){
         randomChange = random([0,1,4]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed off-off-off-on-on");
+        //console.log("Changed off-off-off-on-on");
       }
       else if(onOffArray[0] == false && onOffArray[1] == true && onOffArray[2] == true && onOffArray[3] == false && onOffArray[4] == false){
         randomChange = random([0,3,4]); //temp
         onOffArray[randomChange] = true;
-        console.log("Changed off-off-off-on-on");
+        //console.log("Changed off-off-off-on-on");
       }
       else{
-        console.log("Changed Nothing");
+        //console.log("Changed Nothing");
         setupDone = true;
       }
     }
     
-    /*
-    
-    */
     this.sw1 = new flipSwitch(onOffArray[0], (this.renderer.width *0.05), this.renderer.height*0.2, this.renderer.width);
     this.sw2 = new flipSwitch(onOffArray[1], (this.renderer.width *0.25), this.renderer.height*0.2, this.renderer.width);
     this.sw3 = new flipSwitch(onOffArray[2], (this.renderer.width *0.45), this.renderer.height*0.2, this.renderer.width);
     this.sw4 = new flipSwitch(onOffArray[3], (this.renderer.width *0.65), this.renderer.height*0.2, this.renderer.width);
     this.sw5 = new flipSwitch(onOffArray[4], (this.renderer.width *0.85), this.renderer.height*0.2, this.renderer.width);
-
-    this.gameOver = false;
+    this.resetSwitchGame = new resetSwitch(false, (this.renderer.width *0.85), this.renderer.height*0.75, this.renderer.width);
+    this.winLoss = 0; //-1 -> You lose : 0 -> Ongoing : 1 -> You win
     this.puzzleID = 1;
   }
 
@@ -126,6 +143,15 @@ class flipSwitchGame extends Puzzle{
 
   isSolved() {
     //console.log('Checking if the puzzle is solved')
+    if(this.sw1.onOff === false && this.sw2.onOff === false && this.sw3.onOff === false && this.sw4.onOff === false && this.sw5.onOff === false){
+      this.winLoss = -1;
+      return true;
+    }
+    else if(this.sw1.onOff === true && this.sw2.onOff === true && this.sw3.onOff === true && this.sw4.onOff === true && this.sw5.onOff === true){
+      this.winLoss = 1;
+      return true;
+    }
+    else {return false;}
   }
 
   handleMousePressed(mx, my) {
@@ -140,29 +166,32 @@ class flipSwitchGame extends Puzzle{
     this.sw3.drawSwitch(this.renderer);
     this.sw4.drawSwitch(this.renderer);
     this.sw5.drawSwitch(this.renderer);
+    this.resetSwitchGame.drawResetSwitch(this.renderer);
   }
 
   flipBoard(mx,my){
-    if(this.gameOver != true){
+    if(this.winLoss == 0){
       if(mx > this.sw1.x && mx < this.sw1.x + this.sw1.w && my > this.sw1.y && my < this.sw1.y + this.sw1.h){this.sw1.flip(); this.sw2.flip(); this.sw5.flip();}
       else if(mx > this.sw2.x && mx < this.sw2.x + this.sw2.w && my > this.sw2.y && my < this.sw2.y + this.sw2.h){this.sw1.flip(); this.sw2.flip(); this.sw3.flip();}
       else if(mx > this.sw3.x && mx < this.sw3.x + this.sw3.w && my > this.sw3.y && my < this.sw3.y + this.sw2.h){this.sw2.flip(); this.sw3.flip(); this.sw4.flip();}
       else if(mx > this.sw4.x && mx < this.sw4.x + this.sw4.w && my > this.sw4.y && my < this.sw4.y + this.sw2.h){this.sw3.flip(); this.sw4.flip(); this.sw5.flip();}
       else if(mx > this.sw5.x && mx < this.sw5.x + this.sw5.w && my > this.sw5.y && my < this.sw5.y + this.sw2.h){this.sw4.flip(); this.sw5.flip(); this.sw1.flip();}
     }
+    if(mx > this.resetSwitchGame.x && mx < this.resetSwitchGame.x + this.resetSwitchGame.w && mx > this.resetSwitchGame.y && mx < this.resetSwitchGame.y +this.resetSwitchGame.h){
+      this.resetSwitchGame.resetGame();
+    }
   }
 
   gameState(){
-    if(this.sw1.onOff === false && this.sw2.onOff === false && this.sw3.onOff === false && this.sw4.onOff === false && this.sw5.onOff === false){
-      this.gameOver = true;
+    //this.isSolved();
+    if(this.isSolved() == true && this.winLoss == -1){
       this.renderer.stroke('white');
       this.renderer.strokeWeight(4);
       this.renderer.textSize(this.renderer.width * 0.1);
       this.renderer.fill('black');
       this.renderer.text('You lose', this.renderer.width/2, this.renderer.height/1.25);
     }
-    else if(this.sw1.onOff === true && this.sw2.onOff === true && this.sw3.onOff === true && this.sw4.onOff === true && this.sw5.onOff === true){
-      this.gameOver = true;
+    else if(this.isSolved() == true && this.winLoss == 1){
       this.renderer.stroke('white');
       this.renderer.strokeWeight(4);
       this.renderer.textSize(this.renderer.width *0.1);
@@ -172,11 +201,11 @@ class flipSwitchGame extends Puzzle{
     else{
       this.gameOver = false;
     }
+
   }
 
   resetGameState(){
-    //may be needed later good to have
-    this.gameOver = false;
+    
   }
 
   gameInstructions(){
