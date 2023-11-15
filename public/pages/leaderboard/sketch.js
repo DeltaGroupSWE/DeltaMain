e75b2085e9d745bc8f0e40c9cc9075c028e3aae1
Thomas-Moore81 <tmoore81@kent.edu>
2023-11-15 14:38:26 -0500

    Refactored leaderboard for ease of future edits

--- public/pages/leaderboard/sketch.js
+++ public/pages/leaderboard/sketch.js
@@ -1,107 +1,115 @@
-function preload(){}
 
-function setup(){
-    
-createCanvas(winWidth,winHeight *.9);
+let quitButton;
+let leaderboard;
+let database;
+let playerScores = {};
 
- /*styling button*/
- quitButton = createButton("Return to Menu");
+let endGameButton;
 
- // CSS styles relative to canvas size
- const buttonWidth = winWidth * 0.4; // Adjust the button width
- const buttonHeight = winHeight * 0.1; // Adjust the button height
- const buttonFontSize = winWidth * 0.03; // Adjust the font size
+function setup() {
+  createCanvas(winWidth, winHeight * 0.9);
 
- quitButton.style("background-color: #1A71E6");
- quitButton.style(`padding: ${winHeight * 0.04}px ${winWidth * 0.1}px`);
- quitButton.style("display: inline-block");
- quitButton.style("font-family: Verdana");
- quitButton.style(`margin: ${winHeight * 0.02}px ${winWidth * 0.02}px`);
- quitButton.style(`border-radius: ${winWidth * 0.04}px`);
- quitButton.style(`border: ${winWidth * 0.001}px solid #000000`);
- quitButton.style(`font-size: ${buttonFontSize}px`);
 
- // Center the button within the canvas
- quitButton.position(
-   winWidth / 2 - buttonWidth / 2,
-   3 * (winHeight / 4) - buttonHeight / 2
- );
+  // Styling button
+  quitButton = createButton("Return to Menu");
+  styleButton(quitButton, winWidth * 0.02, winHeight * 0.02, winWidth * 0.01);
+  positionButton(quitButton, winHeight * 0.04, winWidth * -0.04, 3 * (winHeight / 4));
 
- /*mousePressed Event*/
- quitButton.mousePressed(returnToMenu);
+  quitButton.mousePressed(returnToMenu);
 
- // test button for adding score to leaderboard
-  const testLeaderBoardButton = createButton("Test Leaderboard");
-  testLeaderBoardButton.position(20, 20);
-  testLeaderBoardButton.mousePressed(testLeaderboard);
+  leaderboard = new Leaderboard(database);
+
+  endGameButton = createButton("End Game");
+  styleButton(endGameButton, winWidth * 0.02, winHeight * 0.02, winWidth * 0.01);
+  positionButton(endGameButton, winHeight * 0.04, winWidth * -0.04, (winHeight / 2));
+  endGameButton.mousePressed(endGame);
 
 }
 
-function drawLeaderBoard(){
+function draw() {
+  background(200);
+  leaderboard.draw(playerScores);
+  leaderboard.draw();
+}
 
-  const topTextSize = winWidth * 0.13;
-  const topTextX = winWidth / 2;
-  const topTextY = winHeight * (2 / 14);
+class Leaderboard {
+  constructor() {
+    this.topTextSize = winWidth * 0.13;
+    this.topTextX = winWidth / 2;
+    this.topTextY = winHeight * (2 / 14);
 
-  const midTextSize = winWidth * 0.025;
-  const midTextXRank = winWidth / 8;
-  const midTextXName = winWidth / 3;
-  const midTextXTime = winWidth / 1.25;
-  const midTextY = winHeight * (5 / 14);
-
-  textAlign(CENTER,CENTER);
-  textSize(topTextSize);
-  fill(102, 0, 0);
-  text("Leaderboard",topTextX,topTextY);
-  
-  textSize(midTextSize);
-  text("Rank",midTextXRank,midTextY);
-  text("Name",midTextXName,midTextY);
-  text("Time",midTextXTime,midTextY);
-    
-
-  // testing player scores
-  const playerScores = getAllPlayerScores();
-
-  let posY = midTextY + midTextSize; // Start position for scores
-  for (const playerName in playerScores) {
-    const score = playerScores[playerName];
-    posY += midTextSize;
-    text(`${playerName}: ${score}`, midTextXRank, posY);
+    this.midTextSize = winWidth * 0.025;
+    this.midTextXRank = winWidth / 8;
+    this.midTextXName = winWidth / 3;
+    this.midTextXTime = winWidth / 1.25;
+    this.midTextY = winHeight * (5 / 14);
   }
 
-}
+  draw() {
+    textAlign(CENTER, CENTER);
+    textSize(this.topTextSize);
+    fill(102, 0, 0);
+    text("Leaderboard", this.topTextX, this.topTextY);
 
-function draw(){
-background(200);
-drawLeaderBoard();
-}
+    textSize(this.midTextSize);
+    text("Rank", this.midTextXRank, this.midTextY);
+    text("Name", this.midTextXName, this.midTextY);
+    text("Time", this.midTextXTime, this.midTextY);
 
-function returnToMenu(){
-    window.location.href = '../home/index.html';
-}
-
-// function for setting a players score as a cookie
-function setPlayerScore(playerName, score) {
-  document.cookie = `${playerName}=${score}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
-
-}
-
-function getAllPlayerScores(){ 
-  const cookies = document.cookie.split("; ");  
-  const playerScores = {}; 
-
-  for (const cookie of cookies) {
-    const [playerName, score] = cookie.split("=");
-    playerScores[playerName] = parseInt(score);
+    let posY = this.midTextY + this.midTextSize; // Start position for scores
+    for (const playerName in playerScores) {
+      const score = playerScores[playerName];
+      posY += this.midTextSize;
+      text(`${playerName}: ${score}`, this.midTextXRank, posY);
+    }
   }
-
-  return playerScores;
-
 }
 
-function testLeaderboard() {
-  setPlayerScore("TestPlayer", 1500);
-
-  drawLeaderBoard();
+function returnToMenu() {
+  window.location.href = '../home/index.html';
 }
+
+// Helper functions for styling and positioning the button
+function styleButton(button, width, height, fontSize) {
+  button.style("background-color: #1A71E6");
+  button.style(`padding: ${height}px ${width}px`);
+  button.style("display: inline-block");
+  button.style("font-family: Verdana");
+  button.style(`margin: ${height * 0.2}px ${width * 0.2}px`);
+  button.style(`border-radius: ${width * 0.04}px`);
+  button.style(`border: ${width * 0.001}px solid #000000`);
+  button.style(`font-size: ${fontSize}px`);
+}
+
+function positionButton(button, topMargin, leftMargin, yPos) {
+  button.position(winWidth / 2 - button.width / 2 + leftMargin, yPos - topMargin);
+}
+/*
+function endGame() {
+  const playerName = "Player1";
+  const playerScore = 1500;
+
+  // Update playerScores with the new score
+  playerScores[playerName] = playerScore;
+
+  // Add the player score to the database
+  addPlayerScore(playerName, playerScore, database);
+}
+
+// Helper function for styling and positioning the button
+function addPlayerScore(playerName, score, database) {
+  // Get a reference to the "scores" node in your database
+  const scoresRef = ref(database, 'leaderboard/scores');
+
+  // Generate a unique ID for the new score entry
+  const newScoreRef = push(scoresRef);
+
+  // Set the data for the new score entry
+  set(newScoreRef, {
+    playerName: playerName,
+    score: score
+  });
+
+  console.log(`Score added for ${playerName}: ${score}`);
+}
+*/
